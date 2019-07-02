540dbdc7746703f4d9b52aae3b98b048ac30a241
PSLCOL\danayas <davinson.anaya@e-hps.com>
2019-07-02 10:03:20 -0500

    new dialogs

--- src/app/bot/bot.module.ts
+++ src/app/bot/bot.module.ts
@@ -1,5 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
+import UniversalConnector from 'universal-connector-client';
+import { ConsoleAdapter } from './adapters';
+import { BotController } from './controllers';
+import { BalanceDialog, ChangeLanguageDialog, DefaultDialog } from './dialogs';
+import { DialogFlowRecognizer } from './recognizers';
+import { BotHandlerService, BotService } from './services';
 
 @Module({
+  controllers: [
+    BotController,
+  ],
+  imports: [
+    UniversalConnector.UniversalConnectorModule.forRoot(),
+  ],
+  providers: [
+    BotHandlerService,
+    ConsoleAdapter,
+    BotService,
+    DialogFlowRecognizer,
+    DefaultDialog,
+    BalanceDialog,
+    ChangeLanguageDialog,
+  ],
 })
-export class BotModule {}
+export class BotModule  implements NestModule {
+  constructor(private botService: BotService) {
+  }
+
+  public configure() {
+    this.botService.configureDialogs();
+  }
+}
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -10,13 +10,36 @@ dotenv.config({
     `${(process.env.NODE_ENV || 'development')}.env`,
   ),
 });
+process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS || './google.json';
 
 interface IEnvironment {
+  bot: {
+    storage: {
+      collection: string;
+      database: string;
+      uri: string;
+    },
+  };
+  dialogFlow: {
+    authorizationFile: string,
+    projectId: string,
+  };
   env: string;
   port: number;
 }
 
 export const environment = {
+  bot: {
+    storage: {
+      collection: process.env.BOT_STORAGE_COLLECTION,
+      database: process.env.BOT_STORAGE_DATABASE,
+      uri: process.env.BOT_STORAGE_URI,
+    },
+  },
+  dialogFlow: {
+    authorizationFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    projectId: 'nodeconf-kdhahj',
+  },
   env:  process.env.NODE_ENV,
   port: Number(process.env.PORT) || 3000,
 } as IEnvironment;
